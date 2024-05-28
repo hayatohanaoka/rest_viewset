98ac0a5c91199da23e182c5932c52ecabe8010d7
hayatohanaoka <suzugamori.hh@gmail.com>
2024-05-29 01:56:15 +0900

    Add data

--- graphql/faq/models.py
+++ graphql/faq/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name='question', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    question = models.ForeignKey(
+        Question, related_name='answers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:50]
