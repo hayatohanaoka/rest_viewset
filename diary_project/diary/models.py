50755fdfabb81727f8497df5190bbd94fe03f8c6
hayatohanaoka <suzugamori.hh@gmail.com>
2024-06-21 01:46:17 +0900

    Create mutation about diary

--- diary_project/diary/models.py
+++ diary_project/diary/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Diary(models.Model):
+    class Meta:
+        ordering = ('pk',)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- diary_project/diary/schema.py
+++ diary_project/diary/schema.py
@@ -1,12 +1,25 @@
 import graphene
-from graphene_django import DjangoObjectType
-from django.contrib.auth.models import User
 from graphene import relay
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from django.contrib.auth.models import User
+from graphql_relay.node import node
+from django.shortcuts import get_object_or_404
+
+from .models import Diary
 
 class UserType(DjangoObjectType):
     class Meta:
         model = User
         fields = ('id', 'username')
+        interfaces = (relay.Node,)
+
+
+class DiaryType(DjangoObjectType):
+    class Meta:
+        model = Diary
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at', 'user')
+        filter_fields = ('title',)
         interfaces = (relay.Node,)
 
 
@@ -25,14 +38,77 @@ class CreateUser(relay.ClientIDMutation):
         return CreateUser(user=user)
 
 
+class CreateDiary(relay.ClientIDMutation):
+    class Input:
+        title = graphene.String(required=True)
+        content = graphene.String(required=True)
+    
+    diary = graphene.Field(DiaryType)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **inputs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('ログインが必要です')
+        diary = Diary(title=inputs['title'], content=inputs['content'], user=user)
+        diary.save()
+        return CreateDiary(diary=diary)
+
+
+class UpdateDiary(relay.ClientIDMutation):
+    class Input:
+        id = graphene.ID(required=True)
+        title = graphene.String()
+        content = graphene.String()
+    
+    diary = graphene.Field(DiaryType)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **inputs):
+        _, diary_id = node.from_global_id(inputs['id'])
+        user = info.context.user
+        print(user)
+        if user.is_anonymous:
+            raise Exception('ログインが必要です')
+        diary = get_object_or_404(Diary, pk=diary_id, user=user)
+        if 'title' in inputs.keys():
+            diary.title = inputs['title']
+        if 'content' in inputs.keys():
+            diary.content = inputs['content']
+        diary.save()
+        return UpdateDiary(diary=diary)
+
+
+class DeleteDiary(relay.ClientIDMutation):
+    class Input:
+        id = graphene.ID(required=True)
+    
+    success = graphene.Boolean()
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **inputs):
+        _, diary_id = node.from_global_id(inputs['id'])
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('ログインが必要です')
+        diary = get_object_or_404(Diary, pk=diary_id, user=user)
+        diary.delete()
+        return DeleteDiary(success=True)
+
+
 class Query(graphene.ObjectType):
-    hello = graphene.String()
+    all_diaries = DjangoFilterConnectionField(DiaryType)
+    diary = relay.Node.Field(DiaryType)
 
     def resolve_hello(root, info):
         return 'hello'
 
 
 class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
+    create_user  = CreateUser.Field()
+    create_diary = CreateDiary.Field()
+    update_diary = UpdateDiary.Field()
+    delete_diary = DeleteDiary.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
